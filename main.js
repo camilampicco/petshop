602da775b7ebfcf3a3eb098d4283c7d9dd179c21
camilapicco <camilamariapicco@gmail.com>
2022-10-26 08:46:36 +0100

    first js

--- main.js
+++ main.js
@@ -1,50 +1,84 @@
-/*
-let nameInserted = prompt('Please tell us your name!');
-let surnameInserted = prompt('And your surname?');
-
-console.log(nameInserted + ' ' + surnameInserted);
-
-alert('Welcome to our Pet Shop,' + ' ' + nameInserted + ' ' + surnameInserted);
-*/
+//PET SHOP venta de productos
+//Calcular valor final de un producto seleccionado en función de impuestos y descuentos.
 
 
-let number1 = 10;
-let number2 = 23;
-let result = number1 + number2;
-console.log(result);
+//crear un algoritmo con un condicional, usando un ciclo utilizar funciones para realizar las operaciones
 
-let result2 = number1 * number2;
-console.log(result2);
+// const addition = (a, b) => a + b 
+// const substraction = (a, b) => a - b 
 
-
-let number3 = '10';
-let number4 = 20;
-
-let result3 = number3 + number4;
-console.log(result3);
+// const addVat = x => x * 0.20;
 
 
-let firstText = 'Holis, ';
-let secondText = 'Como estas?';
-let result4 = firstText + secondText;
-console.log (result4);
+//declaro las variables
 
 
-let result5 = firstText + 1;
-console.log(result5);
-
-
-const SPACE = ' ';
-let result6 = firstText + SPACE + number3;
-console.log(result6);
+// let priceSoftToyDog = 20;
+// let priceSoftToyCat = 18.5;
+// let priceCatCave = 42.5;
+// let priceCatFood = 15.8;
+// let priceDogFood = 21.2;
+// let priceDiscount = 5;
 
 
 
-let numberA = parseInt(prompt('inserte primer numero'));
-let numberB = parseInt(prompt('inserte segundo numero'));
-
-let resultC = numberA + numberB;
-
-alert(numberA + numberB);
 
 
+
+//declaro la variable  totalPrice por fuera del scope de la funcion
+let totalPrice = 0;
+
+
+//declaro la funcion
+function purchaseProducts () {
+let quantity = 0;
+let price = 0;
+let productId = '';
+
+
+    while (!productId || productId === 0 || productId > 5) { 
+            productId = parseInt(prompt('Which product do you want to shop? \n 1)Soft Dog Toy (£20) \n 2)Soft Cat Toy (£18.5) \n 3)Cat Cave (£42.5) \n 4)Cat Food(£15.8) \n 5)Dog Food (£21.2) '));
+            quantity = parseInt(prompt('How many do you want?'))
+        }
+
+    switch (productId){
+        case 1:
+            productId = 'Soft Dog Toy';
+            price = 20;
+            break;
+        case 2:
+            productId = 'Soft Cat Toy';
+            price = 18.5;
+            break;
+        case 3:
+            productId = 'Cat Cave';
+            price = 42.5;
+            break;
+        case 4:
+            productId = 'Cat Food';
+            price = 15.8;
+            break;
+        case 5:
+            productId = 'Dog Food';
+            price = 21.2;
+            break;
+        default:
+            alert ('Please pick a product to continue');
+            price = 0;
+            quantity = 0;
+            break;
+    }
+    while (!quantity || quantity === 0) {
+        quantity = parseInt(prompt('How many would you like?'));
+    }
+    totalPrice += quantity * price;
+    quantity += quantity;
+
+    alert ('Congratulations, your purchase is ' + productId + ' and you paid £' + totalPrice)
+}
+
+//invoco la funcion
+purchaseProducts ();
+
+//muestro por consola el precio total
+console.log(totalPrice)
