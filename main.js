3ceaaaea6b9ff8b3f148c45e564d77d57f5ba1bd
camilapicco <camilamariapicco@gmail.com>
2022-10-26 07:29:23 +0100

    first changes

---
+++ main.js
@@ -0,0 +1,50 @@
+/*
+let nameInserted = prompt('Please tell us your name!');
+let surnameInserted = prompt('And your surname?');
+
+console.log(nameInserted + ' ' + surnameInserted);
+
+alert('Welcome to our Pet Shop,' + ' ' + nameInserted + ' ' + surnameInserted);
+*/
+
+
+let number1 = 10;
+let number2 = 23;
+let result = number1 + number2;
+console.log(result);
+
+let result2 = number1 * number2;
+console.log(result2);
+
+
+let number3 = '10';
+let number4 = 20;
+
+let result3 = number3 + number4;
+console.log(result3);
+
+
+let firstText = 'Holis, ';
+let secondText = 'Como estas?';
+let result4 = firstText + secondText;
+console.log (result4);
+
+
+let result5 = firstText + 1;
+console.log(result5);
+
+
+const SPACE = ' ';
+let result6 = firstText + SPACE + number3;
+console.log(result6);
+
+
+
+let numberA = parseInt(prompt('inserte primer numero'));
+let numberB = parseInt(prompt('inserte segundo numero'));
+
+let resultC = numberA + numberB;
+
+alert(numberA + numberB);
+
+
