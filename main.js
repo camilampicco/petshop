e5a3ca94d0a5b6d7f896cc26ba55e6c98ba8ebba
camilapicco <camilamariapicco@gmail.com>
2022-11-06 20:11:12 +0000

    to be reviewed

--- main.js
+++ main.js
@@ -1,133 +1,134 @@
-//PET SHOP venta de productos
-
-function purchaseProducts () {
-let quantity = 0;
-let price = 0;
-let productId = '';
-let totalPrice = 0;
-let keepShopping = false;
-let totalQuantity =0;
+// //PET SHOP venta de productos
 
 
-    do { 
-        productId = parseInt(prompt('Which product do you want to shop? \n 1)Soft Dog Toy (£20) \n 2)Soft Cat Toy (£22) \n 3)Cat Cave (£42.5) \n 4)Cat Food(£18) \n 5)Dog Food (£25) '));
-        let validatedProductId = validateProductId (productId)
-        if (productId === 1) {
-            alert('This item is on sale! 20% OFF')
-        }
 
-        quantity = parseInt(prompt('How many do you want?'))
-        let validatedQuantity = validateQuantity (quantity)
-
-    switch (validatedProductId){
-        case 1:
-            validatedProductId = 'Soft Dog Toy';
-            price = 20;
-            break;
-        case 2:
-            validatedProductId = 'Soft Cat Toy';
-            price = 22;
-            break;
-        case 3:
-            validatedProductId = 'Cat Cave';
-            price = 42.5;
-            break;
-        case 4:
-            validatedProductId = 'Cat Food';
-            price = 18;
-            break;
-        case 5:
-            validatedProductId = 'Dog Food';
-            price = 25;
-            break;
-        default:
-            alert ('Please pick a product to continue');
-            price = 0;
-            quantity = 0;
-            break;
-    }
-
-    if (productId === 1) {
-        price = price * 0.8;
-    }
-
-    totalPrice += validatedQuantity * price;
-    totalQuantity += quantity;
-
-    keepShopping = confirm('Do you want to keep shopping?')
-
-} while(keepShopping)
-
-return totalPrice;
-}
+// function purchaseProducts () {
+// let quantity = 0;
+// let price = 0;
+// let productId = '';
+// let totalPrice = 0;
+// let keepShopping = false;
+// let totalQuantity =0;
 
 
-//validacion de producto ingresado
+//     do { 
+//         productId = parseInt(prompt('Which product do you want to shop? \n 1)Soft Dog Toy (£20) \n 2)Soft Cat Toy (£22) \n 3)Cat Cave (£42.5) \n 4)Cat Food(£18) \n 5)Dog Food (£25) '));
+//         let validatedProductId = validateProductId (productId)
+//         if (productId === 1) {
+//             alert('This item is on sale! 20% OFF')
+//         }
 
-function validateProductId (productId) { 
-    while(productId === 0 || productId > 5) {
-            alert ('Please pick a product to continue')
-            productId = parseInt(prompt('Which product do you want to shop? \n 1)Soft Dog Toy (£20) \n 2)Soft Cat Toy (£22) \n 3)Cat Cave (£42.5) \n 4)Cat Food(£18) \n 5)Dog Food (£25) '));
-}
+//         quantity = parseInt(prompt('How many do you want?'))
+//         let validatedQuantity = validateQuantity (quantity)
 
-    return productId;
-}
+//     switch (validatedProductId){
+//         case 1:
+//             validatedProductId = 'Soft Dog Toy';
+//             price = 20;
+//             break;
+//         case 2:
+//             validatedProductId = 'Soft Cat Toy';
+//             price = 22;
+//             break;
+//         case 3:
+//             validatedProductId = 'Cat Cave';
+//             price = 42.5;
+//             break;
+//         case 4:
+//             validatedProductId = 'Cat Food';
+//             price = 18;
+//             break;
+//         case 5:
+//             validatedProductId = 'Dog Food';
+//             price = 25;
+//             break;
+//         default:
+//             alert ('Please pick a product to continue');
+//             price = 0;
+//             quantity = 0;
+//             break;
+//     }
+
+//     if (productId === 1) {
+//         price = price * 0.8;
+//     }
+
+//     totalPrice += validatedQuantity * price;
+//     totalQuantity += quantity;
+
+//     keepShopping = confirm('Do you want to keep shopping?')
+
+// } while(keepShopping)
+
+// return totalPrice;
+// }
 
 
-//validacion para cantidad ingresada
+// //validacion de producto ingresado
 
-function validateQuantity (quantity) { 
-    while(Number.isNaN(quantity) || quantity === 0) {
-        if (quantity != 0){
-            alert ('Please insert a number')
-        } else {
-            alert ('The number should be different than zero')
-        }
-        quantity = parseInt(prompt('How many do you want?'));
-    }
+// function validateProductId (productId) { 
+//     while(productId === 0 || productId > 5) {
+//             alert ('Please pick a product to continue')
+//             productId = parseInt(prompt('Which product do you want to shop? \n 1)Soft Dog Toy (£20) \n 2)Soft Cat Toy (£22) \n 3)Cat Cave (£42.5) \n 4)Cat Food(£18) \n 5)Dog Food (£25) '));
+// }
 
-    return quantity;
-}
+//     return productId;
+// }
 
 
-// creo funcion para calculo de IVA pagaron del precio
+// //validacion para cantidad ingresada
 
-function calculateVATAmount(totalPrice) {
+// function validateQuantity (quantity) { 
+//     while(Number.isNaN(quantity) || quantity === 0) {
+//         if (quantity != 0){
+//             alert ('Please insert a number')
+//         } else {
+//             alert ('The number should be different than zero')
+//         }
+//         quantity = parseInt(prompt('How many do you want?'));
+//     }
+
+//     return quantity;
+// }
+
+
+// // creo funcion para calculo de IVA pagaron del precio
+
+// function calculateVATAmount(totalPrice) {
     
-    vatamount = totalPrice * 0.20;
-    console.log('The VAT for this purchase is: ' + vatamount)
+//     vatamount = totalPrice * 0.20;
+//     console.log('The VAT for this purchase is: ' + vatamount)
 
-    return vatamount;
-}
-
-
-// creo funcion para el calculo de Envio
-
-function calculateDelivery (totalPrice) {
-    let priceOfShipping = 5;
-
-    prompt('Delivery! Which is your address?', 'My Post Code is')
-
-    if (totalPrice >= 60){
-        alert ('You are qualified for free shipping! You now need to pay: £' + totalPrice)
-    } else if (totalPrice < 60 && totalPrice !== 0) { 
-        totalPrice += priceOfShipping
-        alert ('The shipping will cost you £5. You now need to pay: £' + totalPrice) 
-    }
-
-    return totalPrice;
-}
+//     return vatamount;
+// }
 
 
+// // creo funcion para el calculo de Envio
+
+// function calculateDelivery (totalPrice) {
+//     let priceOfShipping = 5;
+
+//     prompt('Delivery! Which is your address?', 'My Post Code is')
+
+//     if (totalPrice >= 60){
+//         alert ('You are qualified for free shipping! You now need to pay: £' + totalPrice)
+//     } else if (totalPrice < 60 && totalPrice !== 0) { 
+//         totalPrice += priceOfShipping
+//         alert ('The shipping will cost you £5. You now need to pay: £' + totalPrice) 
+//     }
+
+//     return totalPrice;
+// }
 
 
-const totalPrice = purchaseProducts();
-calculateDelivery(totalPrice);
-calculateVATAmount(totalPrice);
+
+
+// const totalPrice = purchaseProducts();
+// calculateDelivery(totalPrice);
+// calculateVATAmount(totalPrice);
 
 
 // base de datos de usuarios
-
 
 
 
@@ -137,21 +138,44 @@ calculateVATAmount(totalPrice);
 
 const products = [
     {
+        id: 1,
         name: 'Soft Toy Dog',
-        price: 20
+        price: 20,
+        description: 'individual toy for dogs',
+        size: 'one size',
+        quantity: 1
     },
     {
+        id: 2,
         name: 'Soft Cat Toy',
-        price: 22
-    },    {
+        price: 22,
+        description: 'individual toy for cats',
+        size: 'one size',
+        quantity: 1
+    },    
+    {
+        id: 3,
         name: 'Cat Cave',
-        price: 42.5
-    },    {
+        price: 42.5,
+        description: 'Bowie cave for cats',
+        size: 'one size',
+        quantity: 1
+    },    
+    {
+        id: 4,
         name: 'Cat Food',
-        price: 18
-    },    {
+        price: 18,
+        description: 'Food for indoor cats',
+        size: '1kg',
+        quantity: 1
+    },    
+    {
+        id: 5,
         name: 'Dog Food',
-        price: 25
+        price: 25,
+        description: 'Food for small dogs',
+        size: '1kg',
+        quantity: 1
     },
 ]
 
@@ -203,6 +227,7 @@ const sortByPrice = products.sort((a,b) => {
 console.log(sortByPrice)
 
 
+
 // carrito de compras
 
 const basket = [
@@ -220,23 +245,41 @@ console.log(totalSpent)
 
 
 
-//creacion de usuarios
 
-const names = [];
 
-let newUser = '';
+
+//creacion de usuarios ---->>>> trabajando en esto
+
+const listOfNames = [];
+let inputName = '';
 
 do {
-    if (newUser != ''){
-        alert ('That username is taken, please choose other');
+    inputName = prompt('Please insert your username')
+    if (inputName = ''){
+        alert('The user name cannot be empty' )
+    } else {
+        listOfNames.push(inputName);
+        console.log(inputName)
     }
-    newUser = prompt('Please write your username')
+} while (inputName)
 
-} while (names.includes(newUser))
+const newList = listOfNames.concat
+console.log()
 
-names.push(newUser) //ver bien como hacer para que se sume al array
 
-alert ('The user name has been successfully created!')
+// const names = [];
+// let newUser = ''
 
-console.log(names)
+//     if (newUser != ''){
+//         newUser = prompt('Please write your username')
+//         alert ('That username is taken, please choose other');
+// } while (names.includes(newUser))
+
+// names.push(newUser) //ver bien como hacer para que se sume al array
+
+// alert ('The user name has been successfully created!')
+// console.log(names.join('\n'))
+
+
+
 
